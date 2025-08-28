0ce00e25b7cce6ee28a9d5548e0a7531c84cd2fb
Emir Eroğlu <emireroglu2k@gmail.com>
2025-08-28 05:02:39 +0300

    implement navbar

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+    {id: 'experience', title: 'Experience'},
+    {id: 'education', title: 'Education'},
+    {id: 'projects', title: 'Projects'},
+    {id: 'skills', title: 'Skills'},
+    {id: 'contact', title: 'Contact'},
+]
